4b5ee70d04a29115e1a8b1958d3f10728a01af8f
yaacov perez <perezyaacov0@gmail.com>
2024-07-18 10:48:05 +0200

    correction css

--- script.js
+++ script.js
@@ -17,6 +17,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    document.addEventListener('DOMContentLoaded', () => {
+        const links = document.querySelectorAll('nav a');
+    
+        links.forEach(link => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault();
+                const targetId = link.getAttribute('href').substring(1);
+                const targetSection = document.getElementById(targetId);
+    
+                targetSection.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            });
+        });
+    });
+    
     document.getElementById('copyMessageButton').addEventListener('click', function() {
         const message = document.getElementById('preconfiguredMessage').innerText;
         navigator.clipboard.writeText(message).then(function() {
